51c469c559bc99c9afb846d34f1977fec5463023
Alan Luo <i@iltc.io>
2020-10-11 10:07:53 -0400

    Nosetests Configuration (#21)
    * Add setup and demo test case
    
    * Update README.md

---
+++ tests/test_demo.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+class TestDemo(unittest.TestCase):
+    """ Test Cases for Demo """
+
+    @classmethod
+    def setUpClass(cls):
+        """ This runs once before the entire test suite """
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """ This runs once after the entire test suite """
+        pass
+
+    def setUp(self):
+        """ This runs before each test """
+        pass
+
+    def tearDown(self):
+        """ This runs after each test """
+        pass
+
+    def test_demo(self):
+        """ Test Demo """
+        self.assertTrue(True)
