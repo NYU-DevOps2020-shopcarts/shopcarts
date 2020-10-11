e6c12b7b1641576278403216e6b68952917ba855
KHafner <KHafner@users.noreply.github.com>
2020-10-11 11:16:07 -0400

    setup for models.py (#22)
    * setup for models.py
    
    * updated readme
    
    * added test cases for models
    
    * renamed uid to user_id
    
    Co-authored-by: Alan Luo <i@iltc.io>

---
+++ service/models.py
@@ -0,0 +1,182 @@
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""
+Models for Shopcarts
+All of the models are stored in this module
+Models
+------
+
+Shopcart -
+Attributes:
+-----------
+id (int) - shop cart id
+user_id (int) - user id
+create_time (DateTime) - the time this shopcart was created
+update_time (DateTime) - the time this shopcart was updated
+
+ShopcartItem - Contains product information for an item in a shopcart
+Attributes:
+-----------
+id (int) - for index purpose
+sid (int) - shop cart id
+sku (int) - product id
+name (string) - product name
+price (float) - price for one
+amount (int) - number of product
+create_time (DateTime) - the time this product was created
+update_time (DateTime) - the time this product was updated
+"""
+
+import logging
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+# Create the SQLAlchemy object to be initialized later in init_db()
+db = SQLAlchemy()
+
+class DataValidationError(Exception):
+    """ Used for an data validation errors when deserializing """
+
+    pass
+
+class Shopcart(db.Model):
+    """
+    Class that represents a Shopcart
+    This version uses a relational database for persistence which is hidden
+    from us by SQLAlchemy's object relational mappings (ORM)
+    """
+
+    logger = logging.getLogger(__name__)
+    app = None
+
+    ##################################################
+    # Shopcart Table Schema
+    ##################################################
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    create_time = db.Column(db.DateTime)
+    update_time = db.Column(db.DateTime)
+
+    ##################################################
+    # INSTANCE METHODS
+    ##################################################
+
+    def serialize(self):
+        """ Serializes a Shopcart into a dictionary """
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "create_time": self.create_time,
+            "update_time": self.update_time,
+        }
+
+    def deserialize(self, data: dict):
+        """
+        Deserializes a Shopcart from a dictionary
+        :param data: a dictionary of attributes
+        :type data: dict
+        :return: a reference to self
+        :rtype: Shopcart
+        """
+        try:
+            self.id = data["id"]
+            self.user_id = data["user_id"]
+            self.create_time = data["create_time"]
+            self.update_time = data["update_time"]
+        except KeyError as error:
+            raise DataValidationError("Invalid shopcart: missing " + error.args[0])
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid shopcart: body of request contained bad or no data"
+            )
+        return self
+
+class ShopcartItem(db.Model):
+    """
+    Class that represents a ShopcartItem
+    This version uses a relational database for persistence which is hidden
+    from us by SQLAlchemy's object relational mappings (ORM)
+    """
+
+    logger = logging.getLogger(__name__)
+    app = None
+
+    ##################################################
+    # Shopcart Table Schema
+    ##################################################
+    id = db.Column(db.Integer, primary_key=True)
+    sid = db.Column(db.Integer)
+    sku = db.Column(db.Integer)
+    name = db.Column(db.String)
+    price = db.Column(db.Float)
+    amount = db.Column(db.Integer)
+    create_time = db.Column(db.DateTime)
+    update_time = db.Column(db.DateTime)
+
+    ##################################################
+    # INSTANCE METHODS
+    ##################################################
+
+    def serialize(self):
+        """ Serializes a Shopcart into a dictionary """
+        return {
+            "id": self.id,
+            "sid": self.sid,
+            "sku": self.sku,
+            "name": self.name,
+            "price": self.price,
+            "amount": self.amount,
+            "create_time": self.create_time,
+            "update_time": self.update_time,
+        }
+
+    def deserialize(self, data: dict):
+        """
+        Deserializes a ShopcartItem from a dictionary
+        :param data: a dictionary of attributes
+        :type data: dict
+        :return: a reference to self
+        :rtype: ShopcartItem
+        """
+        try:
+            self.id = data["id"]
+            self.sid = data["sid"]
+            self.sku = data["sku"]
+            self.name = data["name"]
+            self.price = data["price"]
+            self.amount = data["amount"]
+            self.create_time = data["create_time"]
+            self.update_time = data["update_time"]
+        except KeyError as error:
+            raise DataValidationError("Invalid shopcart item: missing " + error.args[0])
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid shopcart item: body of request contained bad or no data"
+            )
+        return self
+
+
+
+def init_db(cls, app):
+    """Initializes the database session
+    :param app: the Flask app
+    :type data: Flask
+    """
+    cls.logger.info("Initializing database")
+    cls.app = app
+    # This is where we initialize SQLAlchemy from the Flask app
+    db.init_app(app)
+    app.app_context().push()
+    db.create_all()  # make our sqlalchemy tables
+    
---
+++ tests/test_models.py
@@ -0,0 +1,189 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Test cases for Shopcart and ShopcartItem Models
+Test cases can be run with:
+  nosetests
+  coverage report -m
+"""
+
+import unittest
+import os
+from datetime import datetime
+from service.models import Shopcart, ShopcartItem, DataValidationError, db
+from service import app
+
+DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///../db/test.db")
+
+######################################################################
+#  T E S T   C A S E S
+######################################################################
+class TestShopcarts(unittest.TestCase):
+    """ Test Cases for Shopcarts """
+
+    @classmethod
+    def setUpClass(cls):
+        """ These run once before Test suite """
+        app.debug = False
+        # Set up the test database
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+
+    @classmethod
+    def tearDownClass(cls):
+        """ These run once after Test suite """
+        pass
+
+    def setUp(self):
+        Shopcart.init_db(app)
+        db.drop_all()  # clean up the last tests
+        db.create_all()  # make our sqlalchemy tables
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_create_a_shopcart(self):
+        """ Create a shopcart and assert that it exists """
+        date_time = datetime.now()
+        shopcart = Shopcart(user_id=101, create_time=date_time, update_time=date_time)
+        self.assertTrue(shopcart != None)
+        self.assertEqual(shopcart.id, None)
+        self.assertEqual(shopcart.user_id, 101)
+        self.assertEqual(shopcart.create_time, date_time)
+        self.assertEqual(shopcart.update_time, date_time)
+
+    def test_serialize_a_shopcart(self):
+        """ Test serialization of a Shopcart """
+        date_time = datetime.now()
+        shopcart = Shopcart(user_id=101, create_time=date_time, update_time=date_time)
+        data = shopcart.serialize()
+        self.assertNotEqual(data, None)
+        self.assertIn("id", data)
+        self.assertEqual(data["id"], None)
+        self.assertIn("user_id", data)
+        self.assertEqual(data["user_id"], 101)
+        self.assertIn("create_time", data)
+        self.assertEqual(data["create_time"], date_time)
+        self.assertIn("update_time", data)
+        self.assertEqual(data["update_time"], date_time)
+
+    def test_deserialize_a_shopcart(self):
+        """ Test deserialization of a Shopcart """
+        date_time = datetime.now()
+        data = {"id": 1, "user_id": 120, "create_time": date_time, "update_time": date_time}
+        shopcart = Shopcart()
+        shopcart.deserialize(data)
+        self.assertNotEqual(shopcart, None)
+        self.assertEqual(shopcart.id, None)
+        self.assertEqual(shopcart.user_id, 120)
+        self.assertEqual(shopcart.create_time, date_time)
+        self.assertEqual(shopcart.update_time, date_time)
+
+    def test_deserialize__shopcart_bad_data(self):
+        """ Test deserialization of bad data """
+        data = "this is not a dictionary"
+        shopcart = Shopcart()
+        self.assertRaises(DataValidationError, shopcart.deserialize, data)
+
+class TestShopcartItems(unittest.TestCase):
+    """ Test Cases for ShopcartItems """
+
+    @classmethod
+    def setUpClass(cls):
+        """ These run once before Test suite """
+        app.debug = False
+        # Set up the test database
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+
+    @classmethod
+    def tearDownClass(cls):
+        """ These run once after Test suite """
+        pass
+
+    def setUp(self):
+        ShopcartItem.init_db(app)
+        db.drop_all()  # clean up the last tests
+        db.create_all()  # make our sqlalchemy tables
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_create_a_shopcartitem(self):
+        """ Create a ShopcartItem and assert that it exists """
+        date_time = datetime.now()
+        shopcartitem = ShopcartItem(sid=100, sku=5000, name="soap", price=2.23,
+                                    amount=3, create_time=date_time, update_time=date_time)
+        self.assertTrue(shopcartitem != None)
+        self.assertEqual(shopcartitem.id, None)
+        self.assertEqual(shopcartitem.sid, 100)
+        self.assertEqual(shopcartitem.sku, 5000)
+        self.assertEqual(shopcartitem.name, "soap")
+        self.assertEqual(shopcartitem.price, 2.23)
+        self.assertEqual(shopcartitem.amount, 3)
+        self.assertEqual(shopcartitem.create_time, date_time)
+        self.assertEqual(shopcartitem.update_time, date_time)
+
+    def test_serialize_a_shopcartitem(self):
+        """ Test serialization of a ShopcartItem """
+        date_time = datetime.now()
+        shopcartitem = ShopcartItem(sid=100, sku=5000, name="soap", price=2.23,
+                                    amount=3, create_time=date_time, update_time=date_time)
+        data = shopcartitem.serialize()
+        self.assertNotEqual(data, None)
+        self.assertIn("id", data)
+        self.assertEqual(data["id"], None)
+        self.assertIn("sid", data)
+        self.assertEqual(data["sid"], 100)
+        self.assertIn("sku", data)
+        self.assertEqual(data["sku"], 5000)
+        self.assertIn("name", data)
+        self.assertEqual(data["name"], "soap")
+        self.assertIn("price", data)
+        self.assertEqual(data["price"], 2.23)
+        self.assertIn("amount", data)
+        self.assertEqual(data["amount"], 3)
+        self.assertIn("create_time", data)
+        self.assertEqual(data["create_time"], date_time)
+        self.assertIn("update_time", data)
+        self.assertEqual(data["update_time"], date_time)
+
+    def test_deserialize_a_shopcartitem(self):
+        """ Test deserialization of a ShopcartItem """
+        date_time = datetime.now()
+        data = {"id": 1, "sid":202, "sku":101, "name":"printer", "price":101.29,
+                "amount":1, "create_time":date_time, "update_time":date_time}
+        shopcartitem = ShopcartItem()
+        shopcartitem.deserialize(data)
+        self.assertNotEqual(shopcartitem, None)
+        self.assertEqual(shopcartitem.id, None)
+        self.assertEqual(shopcartitem.sid, 202)
+        self.assertEqual(shopcartitem.sku, 101)
+        self.assertEqual(shopcartitem.name, "printer")
+        self.assertEqual(shopcartitem.price, 101.29)
+        self.assertEqual(shopcartitem.amount, 1)
+        self.assertEqual(shopcartitem.create_time, date_time)
+        self.assertEqual(shopcartitem.update_time, date_time)
+
+    def test_deserialize_shopcaritem_bad_data(self):
+        """ Test deserialization of bad data """
+        data = "this is not a dictionary"
+        shopcartitem = ShopcartItem()
+        self.assertRaises(DataValidationError, shopcartitem.deserialize, data)
+
+
+######################################################################
+#   M A I N
+######################################################################
+if __name__ == "__main__":
+    unittest.main()
